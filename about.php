43a2fdc597036d4db23cbd9a5e58952e53610fbd
Marsad Rasheed <54879111+MarsadRasheed@users.noreply.github.com>
2021-02-04 13:57:57 +0500

    Add files via upload

---
+++ about.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="about.css?v=<?php echo time(); ?>">
+        <link rel="stylesheet" href="Bootstrap/css/bootstrap.min.css?v=<?php echo time(); ?>">
+        <link rel="stylesheet" href="fontawesome-free-5.15.1-web/css/all.css?v=<?php echo time(); ?>">
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="Bootstrap/js/jquery-3.2.1.slim.min.js"></script>
+        <script src="Bootstrap/js/popper.min.js"></script>
+        <script src="Bootstrap/js/bootstrap.min.js"></script>
+
+        <title>About</title>    
+
+    </head>
+
+    <body>
+
+        
+    <!--     Start of header -->
+
+    
+    <header>
+
+        <div id="topHeader">
+        <div class="container-fluid">
+            <div class="row">
+            <div class="col p-2 top-left-tag" style="margin-left: 85px;" style="padding-left: 15px;" >
+                Free shipping for standard order over $100
+            </div>
+            <div class="col-auto p-2 "  style="margin-right: 75px;" style="padding-right: 15px;">
+                <a href="" class="top-right-tags" style="text-decoration: none;"  >Help & FAQs</a>
+                <a href="http://localhost:8080/project/login.php" class="top-right-tags" style="text-decoration: none;">My Account</a>
+                <a href="" class="top-right-tags" style="text-decoration: none;">EN</a>
+                <a href="" class="top-right-tags" style="text-decoration: none;" style="border-right: 1px solid  rgba(255,255,255,.3);
+                ">USD</a>
+            </div>
+            </div>
+        </div>
+        </div>
+        </header>
+
+
+        <div class="container-fluid main" >
+
+        <nav class="navbar navbar-light bg-light navbar-expand-lg "> 
+
+        <a class="navbar-brand Logo"  href="http://localhost:8080/project/home.php"><img src="images/icons/logo-01.png" alt="Image Logo" ></a>
+        
+        <button data-toggle="collapse" data-target="#navbarToggler" type="button"
+        class="navbar-toggler"><span class="navbar-toggler-icon"></span></button>
+
+        <div class="collapse navbar-collapse" id="navbarToggler"> 
+            <ul class="navbar-nav">
+            <li class="nav-item"><a class="nav-link" href="http://localhost:8080/project/home.php" style="text-decoration: none; padding-left: 60px;">Home</a></li>
+            <li class="nav-item"><a class="nav-link" href="http://localhost:8080/project/store.php" style="text-decoration: none;">Shop</a></li>
+            <li class="nav-item"><a class="nav-link" href="http://localhost:8080/project/features.php" style="text-decoration: none;">Features</a></li>
+            <li class="active" class="nav-item"><a class="nav-link" href="http://localhost:8080/project/about.php" style="text-decoration: none;">About</a></li>
+            <li class="nav-item"><a class="nav-link" href="http://localhost:8080/project/contact.php" style="text-decoration: none;">Contact</a></li>
+            </ul>
+        </div>
+        
+        <div >
+            <ul class="nav justify-content-end">
+            <li class="nav-item"> <a  class="nav-link" href=""><i class="fa fa-search"  ></i> </a> </li>
+            <li class="nav-item"> <a  class="nav-link" href="http://localhost:8080/project/cartList.php"><i class="fa fa-cart-plus"></i> </a> </li>
+            <li class="nav-item" style="padding-right: 60px;"> <a  class="nav-link" href="http://localhost:8080/project/wishList.php"><i class="fa fa-heart"  ></i> </a> </li>
+            </ul>
+        </div>
+
+        </nav>
+
+        </div>
+
+        <!-- End of header -->
+
+        <section class="headerContact">
+            <h2 class="h2Header">About</h2>
+        </section>
+
+        
+        <div class="container" style="padding-top: 100px;">
+            <div class="row">
+                <div class="col-md-8">
+                    <div>
+                        <h5 style="font-weight: bold; font-size: 25px">Our Story</h5>
+                        <p style="color: #888;" class="pt-3 pr-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris consequat consequat enim, non auctor massa ultrices non. Morbi sed odio massa. Quisque at vehicula tellus, sed tincidunt augue. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas varius egestas diam, eu sodales metus scelerisque congue. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas gravida justo eu arcu egestas convallis. Nullam eu erat bibendum, tempus ipsum eget, dictum enim. Donec non neque ut enim dapibus tincidunt vitae nec augue. Suspendisse potenti. Proin ut est diam. Donec condimentum euismod tortor, eget facilisis diam faucibus et. Morbi a tempor elit.
+
+Donec gravida lorem elit, quis condimentum ex semper sit amet. Fusce eget ligula magna. Aliquam aliquam imperdiet sodales. Ut fringilla turpis in vehicula vehicula. Pellentesque congue ac orci ut gravida. Aliquam erat volutpat. Donec iaculis lectus a arcu facilisis, eu sodales lectus sagittis. Etiam pellentesque, magna vel dictum rutrum, neque justo eleifend elit, vel tincidunt erat arcu ut sem. Sed rutrum, turpis ut commodo efficitur, quam velit convallis ipsum, et maximus enim ligula ac ligula.
+
+Any questions? Let us know in store at 8th floor, 379 Hudson St, New York, NY 10018 or call us on (+1) 96 716 6879</p>
+                    </div>
+                </div>
+
+                <div class="col-md-4 pt-4">
+                    <div class="borderAbout">
+                        <div class="hov">
+                            <img class="img1" src="images/about-01.jpg" alt="ABOUT">
+                        </div>
+                    </div>               
+                </div>
+            </div>
+        </div>
+
+
+        <div class="container" style="padding-top: 100px;">
+            <div class="row">
+            <div class="col-md-4 pt-4">
+                    <div class="borderAbout">
+                        <div class="hov">
+                            <img class="img1" src="images/about-02.jpg" alt="ABOUT">
+                        </div>
+                    </div>               
+                </div>
+
+                <div class="col-md-8" style="padding-left: 100px;">
+                    <div>
+                        <h5 style="font-weight: bold; font-size: 25px">Our Missions</h5>
+                        <p style="color: #888;" class="pt-3">Mauris non lacinia magna. Sed nec lobortis dolor. Vestibulum rhoncus dignissim risus, 
+                        sed consectetur erat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac 
+                        turpis egestas. Nullam maximus mauris sit amet odio convallis, in pharetra magna gravida. Praesent 
+                        sed nunc fermentum mi molestie tempor. Morbi vitae viverra odio. Pellentesque ac velit egestas, 
+                        luctus arcu non, laoreet mauris. Sed in ipsum tempor, consequat odio in, porttitor ante. Ut mauris 
+                        ligula, volutpat in sodales in, porta non odio. Pellentesque tempor urna vitae mi vestibulum, nec 
+                        venenatis nulla lobortis. Proin at gravida ante. Mauris auctor purus at lacus maximus euismod. 
+                        Pellentesque vulputate massa ut nisl hendrerit, eget elementum libero iaculis.</p>
+                    </div>
+
+                    <div class="saying mt-5">
+                        <p style="color: #888;" class="pl-4">Creativity is just connecting things. When you ask creative people how they did something,
+                            they feel a little guilty because they didn't really do it, they just saw something. 
+                            It seemed obvious to them after a while.</p>
+
+                        <p class="pl-4">-Steve Jobs</p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <br>
+        <br>
+        <br>
+        <br>
+        <?php include 'footer.php' ?>
+        
+    </body>
+</html>
