5d312d47dc4da478700467a8ba267330683d8a06
Marsad Rasheed <54879111+MarsadRasheed@users.noreply.github.com>
2021-02-18 18:52:09 +0500

    Add files via upload

---
+++ deleteFromWishList.php
@@ -0,0 +1,16 @@
+<?php
+    $mySql = new mysqli('localhost','root','1234','project') or die($mySql->connect_error);
+    $table = 'wishlist';
+    $id = $_POST["hidden_id"];
+    $result = $mySql->query("Delete from $table where id = '$id'") or die($mySql->error);
+
+    if($result){
+        ?>
+        <script>alert('item is removed successfully')</script>
+        <?php
+    } else {
+        echo 'error';
+    }
+
+    header("refresh:0.1; url=wishList.php");
+?>
